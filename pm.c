06dad1e0f836e2b99dc079321b85d6814ec04579
MethodiosZ <methzaxa@gmail.com>
2024-03-11 19:49:53 +0200

    editted pm file

--- pm.c
+++ pm.c
@@ -7,29 +7,25 @@ int main(int argc, char *argv[]){
     char key[32],buffer[2048];
     char* value;
     int i=0,j,check=0;
-    if(!strcmp(argv[1],"save")){
+    if(!strcmp(argv[1],"add")){
         fptr = fopen(argv[3],"a+");
         printf("Enter password: ");
         scanf("%s",key);
-        fprintf(fptr,"%s,%s,%s\n",argv[4],argv[5],argv[6]);
+        fprintf(fptr,"%s,%s\n",argv[4],argv[5]);
         fclose(fptr);
-    } else if(!strcmp(argv[1],"get")){
+    } else if(!strcmp(argv[1],"read")){
         fptr = fopen(argv[3],"r");
         printf("Enter password: ");
         scanf("%s",key);
         while(fgets(buffer,1024,fptr)){
             j=0;
-            i++;
             value= strtok(buffer,",");
             while(value){
                 if(j==0){
                     if(!strcmp(value,argv[4])) check=1;
                 }
                 if(j==1&&check){
-                    printf("Username: %s\n",value);
-                }
-                if(j==2&&check){
-                    printf("Password: %s\n",value);
+                    printf("Key: %s has value: %s\n",argv[4],value);
                     check=0;
                 }
                 value = strtok(NULL,",");
@@ -37,6 +33,11 @@ int main(int argc, char *argv[]){
             }
         }
         fclose(fptr);
+    } else if(!strcmp(argv[1],"range-read")){
+        fptr = fopen(argv[3],"r");
+        printf("Enter password: ");
+        scanf("%s",key);
+
     } else {
         puts("Error: Command Not Found");
     }
